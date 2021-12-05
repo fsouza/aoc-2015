8b54fe28804f66e0ae8addcbba4af1dfdfecb301
francisco souza <fsouza@users.noreply.github.com>
2021-12-05 00:56:37 -0500

    day10: add caching
    Turns out the input is long, but repeats digits, so by processing digits
    individually, I win.
    
    To make it perfect, I'd use multicore ocaml to process the different
    inputs (in that case I'd probably create a set from the input string, as
    the cache wouldn't be as effective). The problem is that multicore ocaml
    doesn't work on arm64 yet.

--- day10/day10.ml
+++ day10/day10.ml
@@ -1,3 +1,6 @@
+open StdLabels
+open MoreLabels
+
 let new_acc = Printf.sprintf "%s%d%c"
 
 let look_and_say str =
@@ -23,11 +26,26 @@ let rec take n seq () =
     | Nil -> Nil
     | Cons (v, seq) -> Cons (v, take (n - 1) seq)
 
+let run cache times str =
+  match Hashtbl.find_opt cache str with
+  | Some result -> result
+  | None ->
+      let result =
+        str
+        |> look_and_say_seq
+        |> take times
+        |> Seq.fold_left (fun _ el -> el) ""
+        |> String.length
+      in
+      Hashtbl.replace ~key:str ~data:result cache;
+      result
+
 let () =
   let times = Sys.argv.(1) |> int_of_string in
+  let cache = Hashtbl.create times in
   read_line ()
-  |> look_and_say_seq
-  |> take times
-  |> Seq.fold_left (fun _ el -> el) ""
-  |> String.length
+  |> String.to_seq
+  |> Seq.map (String.make 1)
+  |> Seq.map (run cache times)
+  |> Seq.fold_left ( + ) 0
   |> Printf.printf "%d\n"
