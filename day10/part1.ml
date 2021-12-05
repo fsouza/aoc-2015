aa86b8fd27cd7b2d14a7c2b757bc6f8963ec9aa2
francisco souza <fsouza@users.noreply.github.com>
2021-12-04 22:23:26 -0500

    Day 10 part 1

---
+++ day10/part1.ml
@@ -0,0 +1,32 @@
+let new_acc = Printf.sprintf "%s%d%c"
+
+let look_and_say str =
+  let length = String.length str in
+  let rec loop acc idx count curr =
+    if idx >= length then new_acc acc count curr
+    else
+      let ch = str.[idx] in
+      if Char.equal ch curr then loop acc (idx + 1) (count + 1) curr
+      else loop (new_acc acc count curr) (idx + 1) 1 ch
+  in
+  loop "" 1 1 str.[0]
+
+let rec look_and_say_seq str () =
+  let next = look_and_say str in
+  Seq.Cons (next, look_and_say_seq next)
+
+let rec take n seq () =
+  let open Seq in
+  if n = 0 then Nil
+  else
+    match seq () with
+    | Nil -> Nil
+    | Cons (v, seq) -> Cons (v, take (n - 1) seq)
+
+let () =
+  read_line ()
+  |> look_and_say_seq
+  |> take 40
+  |> Seq.fold_left (fun _ el -> el) ""
+  |> String.length
+  |> Printf.printf "%d\n"
