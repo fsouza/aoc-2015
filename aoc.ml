61ab2507e023e7d74152f0649ac2eb7d2bcd7a59
francisco souza <fsouza@users.noreply.github.com>
2021-12-11 19:11:18 -0500

    Bring zip
    It'll be useful in Day 18

--- aoc.ml
+++ aoc.ml
@@ -1,11 +1,16 @@
+open Seq
+
 let rec stdin () =
-  let open Seq in
   try
     let line = read_line () in
     Cons (line, stdin)
   with End_of_file -> Nil
 
 let nat =
-  let open Seq in
   let rec nat' n () = Cons (n, nat' (n + 1)) in
   nat' 0
+
+let rec zip seq1 seq2 () =
+  match (seq1 (), seq2 ()) with
+  | Cons (v1, seq1), Cons (v2, seq2) -> Cons ((v1, v2), zip seq1 seq2)
+  | _ -> Nil
--- aoc.mli
+++ aoc.mli
@@ -1,2 +1,3 @@
 val stdin : string Seq.t
 val nat : int Seq.t
+val zip : 'a Seq.t -> 'b Seq.t -> ('a * 'b) Seq.t
