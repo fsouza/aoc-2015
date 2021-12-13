893d36c5c0f4c1670ce029e240e6e00ffdcd6da2
francisco souza <fsouza@users.noreply.github.com>
2021-12-12 22:47:09 -0500

    Day 20 part 2

---
+++ day20/part2.ml
@@ -0,0 +1,23 @@
+let divisors_of n =
+  let open Seq in
+  let rec divisors_of' i () =
+    if i * i > n || i > 50 then Nil
+    else if n mod i = 0 then
+      let other = n / i in
+      let v = if other > i then i + other else i in
+      Cons (v, divisors_of' (i + 1))
+    else divisors_of' (i + 1) ()
+  in
+  divisors_of' 1
+
+let calc_presents house_number =
+  house_number |> divisors_of |> Seq.map (( * ) 11) |> Seq.fold_left ( + ) 0
+
+let find_answer target =
+  let rec find_answer' house_number =
+    if calc_presents house_number >= target then house_number
+    else find_answer' (house_number + 1)
+  in
+  find_answer' 1
+
+let () = read_line () |> int_of_string |> find_answer |> Printf.printf "%d\n"
